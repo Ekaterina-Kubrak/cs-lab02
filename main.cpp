32e216b95485afbdb4e9998428f41a3e23fd18e4
Alice (KubrakEA) <alice@example.com>
2020-03-23 15:15:18 +0300

    code: добавлен поиск максимума

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,10 +7,24 @@ int main()
 {
     cout << "Enter A and B: ";
     int a, b;
+    vector<double>znach(4);
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+    znach[0] = a + b;
+    znach[1] = a - b;
+    znach[2] = a * b;
+    znach[3] = a / b;
+    double max = znach[0];
+    for(size_t i = 0;i < 4; i++)
+    {
+        if(max < znach[i + 1])
+        {
+            max = znach[i + 1];
+        }
+    }
+    cout << "Max = " << max << '\n';
     return 0;
 }
